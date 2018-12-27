c63223304da34dc548c2b3d7cd580dab97a768cd
SieneDeKeyser <de_keyser_siene@hotmail.com>
2018-12-27 15:45:35 +0100

    Builder

---
+++ TripTime.Infrastructure/Builders/Builder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripTime.Infrastructure.Builders
+{
+   public abstract class Builder<T> where T: new() 
+    {
+        public T Build() {
+            return new T();
+        }
+    }
+}
