9d25c637df39a95c9cbe0f7ea977d9c71418c6b4
SieneDeKeyser <de_keyser_siene@hotmail.com>
2018-12-28 15:28:01 +0100

    hotels

---
+++ TripTime.Service/Trips/Hotels/IHotelService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TripTime.Domain.Trips;
+
+namespace TripTime.Service.Trips.Hotels
+{
+    public interface IHotelService
+    {
+        Task<string> Create(Hotel trip);
+        Task<Hotel> GetHotelById(Guid id);
+        Task<List<Hotel>> GetAllHotels();
+    }
+}
